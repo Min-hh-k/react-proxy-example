d9821b3ff89097023097e4078b9bf26247db6c1c
Min Hyeok <96058996+Min-hh-k@users.noreply.github.com>
2023-02-06 17:57:40 +0900

    2개를 합쳐서 proxy 방법 주석

--- my-app/src/setupProxy.js
+++ my-app/src/setupProxy.js
@@ -1,18 +1,32 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
-    '/api2', //proxy가 필요한 path parameter를 입력합니다.
+    "/api2", //proxy가 필요한 path parameter를 입력합니다.
     createProxyMiddleware({
-      target: 'http://localhost:3070', //타겟이 되는 api url를 입력합니다.
+      target: "http://localhost:3070", //타겟이 되는 api url를 입력합니다.
       changeOrigin: true, //대상 서버 구성에 따라 호스트 헤더가 변경되도록 설정하는 부분입니다.
     })
   );
   app.use(
-    '/api', //proxy가 필요한 path parameter를 입력합니다.
+    "/api", //proxy가 필요한 path parameter를 입력합니다.
     createProxyMiddleware({
-      target: 'http://localhost:3080', //타겟이 되는 api url를 입력합니다.
+      target: "http://localhost:3080", //타겟이 되는 api url를 입력합니다.
       changeOrigin: true, //대상 서버 구성에 따라 호스트 헤더가 변경되도록 설정하는 부분입니다.
     })
   );
 };
+
+//! 2개를 합쳐서 한번에 사용 하는 방법
+// module.exports = function (app) {
+//   app.use(
+//     ["/api", "/api2"],
+//     createProxyMiddleware({
+//       target: "http://localhost:3080",
+//       changeOrigin: true,
+//       router: {
+//         "/api2": "http://localhost:3070",
+//       },
+//     })
+//   );
+// };
